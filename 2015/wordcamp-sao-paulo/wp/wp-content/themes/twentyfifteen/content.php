bf8e011eb9e8b9c8aaf9e1e0d73cfd297e480aa3
Fernando Daciuk <f.daciuk@gmail.com>
2015-11-27 20:45:59 -0200

    Add content file

---
+++ 2015/wordcamp-sao-paulo/wp/wp-content/themes/twentyfifteen/content.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+require_once get_template_directory() . '/vendor/autoload.php';
+$id = get_the_ID();
+$views = (int) get_post_meta( $id, 'views', true );
+$views++;
+update_post_meta( $id, 'views', $views );
+
+$emitter = new SocketIO\Emitter();
+$emitter->emit( 'postviews', $id, $views );
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		(<span data-js="views"><?php echo $views ?></span>) views
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s', 'twentyfifteen' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php
+		// Author bio.
+		if ( is_single() && get_the_author_meta( 'description' ) ) :
+			get_template_part( 'author-bio' );
+		endif;
+	?>
+
+	<footer class="entry-footer">
+		<?php twentyfifteen_entry_meta(); ?>
+		<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
+
+<script src="http://localhost:3000/socket.io/socket.io.js"></script>
+<script>
+(function() {
+  'use strict';
+
+  var socket = io('localhost:3000');
+  socket.on('postviews', function(id, views) {
+    document.querySelector('[data-js="views"]').textContent = views;
+  });
+})();
+</script>
